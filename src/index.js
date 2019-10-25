b97cae602055a77e70b3ac2ed23e4dc659306094
BananaDeveloper <livio.farago@gmail.com>
2019-10-25 13:57:24 +0200

    refact getFormData. Created getFormFields.
    todo: comments on getFormData must be updated

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { validationErrors }     from './modules/validationErrors';
 import { constructorFn }        from './modules/constructor';
 import { destroy }              from './modules/destroy';
 import { getFormData }          from './modules/getFormData';
+import { getFormFields }          from './modules/getFormFields';
 import { init }                 from './modules/init';
 import { validateField }        from './modules/validateField';
 import { validateForm }         from './modules/validateForm';
@@ -24,8 +25,16 @@ class Form {
         destroy.call(this);
     }
     
+    /**
+     * @method getFormData
+     * @description @simplySayHi What should I put here?
+     * 
+     * @returns @simplySayHi what should I put here?
+     */
     getFormData(){
-        return getFormData.call(this);
+        const formFields = getFormFields( this.formEl );
+
+        return this.options.formOptions.getFormData( formFields );
     }
 
     init(){
