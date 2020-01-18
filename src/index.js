da3e09f0426b28299b2e0fe95a836bf5ec31805f
Valerio Di Punzio <sayhi@valeriodipunzio.com>
2020-01-18 15:30:01 +0100

    v3.2.1

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { init }                 from './modules/init';
 import { validateField }        from './modules/validateField';
 import { validateForm }         from './modules/validateForm';
 
-const version = '3.2.0';
+const version = '3.2.1';
 
 class Form {
 
