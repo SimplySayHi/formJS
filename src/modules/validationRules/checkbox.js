6710e7f22278fd2ea4539158a09b13ee2c099239
Valerio Di Punzio <sayhi@valeriodipunzio.com>
2020-09-03 11:47:32 +0200

    minor improvements

--- src/modules/validationRules/checkbox.js
+++ src/modules/validationRules/checkbox.js
@@ -3,5 +3,5 @@ import { checks } from './checks';
 
 export const checkbox = function( value, fieldEl ){
     const dataChecksEl = fieldEl.closest('form').querySelector('[name="' + fieldEl.name + '"][data-checks]');
-    return dataChecksEl ? checks(dataChecksEl): { result: fieldEl.checked };
+    return dataChecksEl ? checks(dataChecksEl) : { result: fieldEl.checked };
 }
