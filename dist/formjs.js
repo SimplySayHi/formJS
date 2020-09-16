435cf9184b0b4dee1b0d76fe338750d7d2bfbbba
Valerio Di Punzio <sayhi@valeriodipunzio.com>
2020-09-16 14:36:58 +0200

    Minor improvement in constructor fn

--- dist/formjs.js
+++ dist/formjs.js
@@ -562,7 +562,7 @@ var Form = function() {
                 if (!(instance instanceof Constructor)) throw new TypeError("Cannot call a class as a function");
             }(this, Form), function(self, formEl, optionsObj) {
                 var argsL = arguments.length, checkFormElem = checkFormEl(formEl);
-                if (0 === argsL || argsL > 0 && !formEl) throw new Error('First argument "formEl" is missing or falsy!');
+                if (1 === argsL || argsL > 1 && !formEl) throw new Error('First argument "formEl" is missing or falsy!');
                 if (isNodeList(formEl)) throw new Error('First argument "formEl" must be a single DOM node or a form CSS selector, not a NodeList!');
                 if (!checkFormElem.result) throw new Error('First argument "formEl" is not a DOM node nor a form CSS selector!');
                 self.formEl = checkFormElem.element, self.formEl.formjs = self, self.options = mergeObjects({}, self.constructor.prototype.options, optionsObj);
